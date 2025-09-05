e95e168c5fa7e8ceebcee7908431b0f7b65b9ff6
encrypt2004 <btech10879.23@bitmesra.ac.in>
2025-09-05 19:27:18 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,37 @@
-import React from 'react'
-import Navbar from './Components/Navbar/Navbar'
-import Hero from './Components/Hero/Hero'
-import Programs from './Components/Programs/Programs'
-import Title from './Components/Title/Title'
-import About from './Components/About/About'
-import Campus from './Components/Campus/Campus'
-import Testimonials from './Components/Testimonials/Testimonials'
-import Contact from './Components/Contact/Contact'
-import Footer from './Components/Footer/Footer'
-import VideoPlayer from './Components/VideoPlayer/VideoPlayer'
+import React, { useState } from 'react'; // ✅ include useState here
+import Navbar from './Components/Navbar/Navbar';
+import Hero from './Components/Hero/Hero';
+import Programs from './Components/Programs/Programs';
+import Title from './Components/Title/Title';
+import About from './Components/About/About';
+import Campus from './Components/Campus/Campus';
+import Testimonials from './Components/Testimonials/Testimonials';
+import Contact from './Components/Contact/Contact';
+import Footer from './Components/Footer/Footer';
+// import VideoPlayer from './Components/VideoPlayer/VideoPlayer'
+
 const App = () => {
+  const [playState, setPlayState] = useState(false);
 
-
-    const [playState,setPlayState] = useState(false);
-    return (
+  return (
     <div>
-      <Navbar/>
-      <Hero/>
+      <Navbar />
+      <Hero />
       <div className="container">
-        <Title subTitle='Our PROGRAM' title='What we offer'/>
-         <Programs/>
-         <About setPlayState={setPlayState} />
-         <Title subTitle='Gallery' title='Campus Photos'/>
-         <Campus/>
-            <Title subTitle='TESTIMONIALS' title='What Students Says'/>
-            <Testimonials/>
-            <Title subTitle='Contact Us' title='Get In Touch'/>
-            <Contact/>
-            <Footer/>
+        <Title subTitle='Our PROGRAM' title='What we offer' />
+        <Programs />
+        <About setPlayState={setPlayState} />
+        <Title subTitle='Gallery' title='Campus Photos' />
+        <Campus />
+        <Title subTitle='TESTIMONIALS' title='What Students Says' />
+        <Testimonials />
+        <Title subTitle='Contact Us' title='Get In Touch' />
+        <Contact />
+        <Footer />
       </div>
-    <VideoPlayer /> playState={playState},{setPlayState}>
+      {/* <VideoPlayer playState={playState} setPlayState={setPlayState} /> */}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
